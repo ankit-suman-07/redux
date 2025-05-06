5e882fe910ee9ec1c2a9c5e3dea57c234998ddea
Ankit Suman <105882103+ankit-suman-07@users.noreply.github.com>
2025-05-07 02:12:19 +0530

    Create store & wrap App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Provider } from 'react-redux'
 import './App.css'
+import store from './store/store'
 
 function App() {
 
   return (
     <>
+    <Provider store={store}>
       App
+    </Provider>
     </>
   )
 }
