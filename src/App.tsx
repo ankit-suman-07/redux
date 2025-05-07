4ce51aabdbe64993756234951e5ae716d122e08b
Ankit Suman <105882103+ankit-suman-07@users.noreply.github.com>
2025-05-07 14:03:09 +0530

    Implement form

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Provider } from 'react-redux'
 import './App.css'
 import store from './store/store'
+import { Container, FormControl, InputLabel, Typography } from '@mui/material'
+import AddHabitForm from './components/add-habit-form'
 
 function App() {
 
   return (
     <>
     <Provider store={store}>
-      App
+      <Container maxWidth="md" >
+        <AddHabitForm />
+      </Container>
     </Provider>
     </>
   )
