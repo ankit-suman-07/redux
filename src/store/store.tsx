b01bdfb549bea16f43f576b9dd24255b5c6ea034
Ankit Suman <105882103+ankit-suman-07@users.noreply.github.com>
2025-05-07 12:40:04 +0530

    Store implemented

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,5 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import habitsReducer from "./habit-slice";
 
-export default configureStore({
-    reducer: {},
+const store = configureStore({
+    reducer: {
+      habits: habitsReducer,
+    },
   })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = ReturnType<typeof store.dispatch>
+export default store;
